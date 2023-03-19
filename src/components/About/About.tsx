f0e55def761d75bfb1a010c562a8982a3bb85172
Ateo101 <nikmail242@gmail.com>
2023-03-19 19:11:12 -0300

    reviews content added

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -5,46 +5,49 @@ import rightSert1 from '../../assets/about/right/sert1.jpg'
 import rightSert2 from '../../assets/about/right/sert2.jpg'
 import rightSert3 from '../../assets/about/right/sert3.jpg'
 import rightSert4 from '../../assets/about/right/sert4.jpg'
-import rightFeedback1 from '../../assets/about/right/feedback1.jpeg'
+import rightFeedback1 from '../../assets/about/right/feedback1.jpg'
+import rightFeedback2 from '../../assets/about/right/feedback2.jpg'
+import rightFeedback3 from '../../assets/about/right/feedback3.jpg'
+import rightFeedback4 from '../../assets/about/right/feedback4.jpg'
 import ModalFunc from "../../common/Modal/Modal";
 
 export const About = () => {
 
     const imagesRight2 = [
         {
-            "text": "Description of image",
+            "text": "Certificate 1",
             "link": rightSert1,
         },
         {
-            "text": "Description of image",
+            "text": "Certificate 2",
             "link": rightSert2,
         },
         {
-            "text": "Description of image",
+            "text": "Certificate 3",
             "link": rightSert3,
         },
         {
-            "text": "Description of image",
+            "text": "Certificate 5",
             "link": rightSert4,
         },
     ]
 
     const imagesRight1 = [
         {
-            "text": "Description of image",
+            "text": "Feedback 1",
             "link": rightFeedback1,
         },
         {
-            "text": "Description of image",
-            "link": rightSert2,
+            "text": "Feedback 2",
+            "link": rightFeedback2,
         },
         {
-            "text": "Description of image",
-            "link": rightSert3,
+            "text": "Feedback 3",
+            "link": rightFeedback3,
         },
         {
-            "text": "Description of image",
-            "link": rightSert4,
+            "text": "Feedback 4",
+            "link": rightFeedback4,
         },
     ]
 
